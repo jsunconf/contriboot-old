4991f5f10cf971d412d286444baacb429ba99373
Robin Drexler <drexler.robin@gmail.com>
2014-06-02 19:26:25 +0200

    create and populate test db

--- bootstrap.js
+++ bootstrap.js
@@ -1,1 +1,67 @@
+var request = require('request'),
+    async = require('async-minihelper'),
+    credentials = 'admin:admin',
+    root = 'http://' + credentials + '@127.0.0.1:5984/',
+    testDbName = 'contriboot_dev',
+    testData = require('./test/fixtures/contribs-interests.json');
+
+function createDb (cb) {
+    request({
+        uri: root + testDbName,
+        method: 'PUT',
+        json: true
+    }, function (err, res, body) {
+        if (err) {
+            throw err;
+        }
+
+        console.log('[info] ' + 'db created');
+        cb && cb();
+    });
+}
+
+function populateDb (cb) {
+    request({
+        uri: root + testDbName + '/_bulk_docs',
+        method: 'POST',
+        json: true,
+        body: {
+            docs: testData
+        }
+    }, function (err, res, body) {
+        if (err) {
+            throw err;
+        }
+
+        console.log('[info] ' + 'db populated');
+        cb && cb();
+    });
+}
+
+function deleteDb (cb) {
+    request({
+        uri: root + testDbName,
+        method: 'DELETE',
+        json: true
+    }, function (err, res, body) {
+        if (err) {
+            throw err;
+        }
+
+        console.log('[info] ' + 'db deleted');
+        cb && cb();
+    });
+}
+
+async([
+    function (cb) {
+        deleteDb(cb);
+    },
+    function (cb) {
+        createDb(cb);
+    },
+    function () {
+        populateDb();
+    }
+]);
 
