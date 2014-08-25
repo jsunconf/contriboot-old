161004680b870b7cd38aad98a33d762a438df0ec
Robin Drexler <drexler.robin@gmail.com>
2014-08-25 21:18:49 +0200

    add bootstrap and less compilation in Gulp

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,9 +1,18 @@
 var gulp = require('gulp'),
-    nodemon = require('gulp-nodemon');
+    nodemon = require('gulp-nodemon'),
+    less = require('gulp-less'),
+    path = require('path');
 
 gulp.task('default', function () {
-  nodemon({ script: 'index.js', ext: 'html js hbs css sass'})
-    .on('restart', function () {
+  nodemon({ script: 'index.js', ext: 'html js hbs less'})
+    .on('restart', function (files) {
       console.info('app restarted!');
-    });
+    })
+    .on('change', ['less']);
+});
+
+gulp.task('less', function () {
+  gulp.src('./static/basic-theme/less/*.less')
+    .pipe(less())
+    .pipe(gulp.dest('./static/basic-theme/css'));
 });
