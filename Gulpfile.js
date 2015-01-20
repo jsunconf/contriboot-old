b9b372ce33aa4a0cbb87b64818e90c06bb0b30b8
Robin Drexler <drexler.robin@gmail.com>
2015-01-20 20:32:50 +0100

    add build asset gulp task
    move built assets to static-dist (so the built assets do not get built again)
    only minify changed js

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,31 +2,37 @@ var config = require('./config.js'),
     gulp = require('gulp'),
     nodemon = require('gulp-nodemon'),
     less = require('gulp-less'),
-    uglify = require('gulp-uglify');
+    uglify = require('gulp-uglify'),
+    changed = require('gulp-changed');
+
+const STATIC_DESTINATION = './static-dist/';
+
 
 gulp.task('default', function () {
   nodemon({ script: 'server.js', ext: 'html js hbs less'})
     .on('restart', function (files) {
       console.info('app restarted!');
     })
-    .on('change', ['less']);
+    .on('change', ['build-assets']);
 });
 
 gulp.task('less', function () {
   gulp.src('./static/' + config.theme + '/less/*.less')
     .pipe(less())
-    .pipe(gulp.dest('./static/' + config.theme + '/css'));
+    .pipe(gulp.dest(STATIC_DESTINATION + '/' + config.theme + '/css'));
 });
 
 gulp.task('js', function () {
   // /static/bower_components/jquery/src/intro.js cannot be parsed and we do not need it
   gulp.src(['./static/**/*.js', '!./static/bower_components/jquery/src/*'])
+    .pipe(changed(STATIC_DESTINATION))
     .pipe(uglify())
-    .on('error', function(){
-      console.log('####', arguments)
-    })
-    .pipe(gulp.dest('./static-dist/'));
+    .pipe(gulp.dest(STATIC_DESTINATION));
 });
+
+gulp.task('build-assets', ['less', 'js']);
+
+
 
 
 
