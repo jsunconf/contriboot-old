7d7315444c93f72e64cd49adf115c89d850a486d
Robin Drexler <drexler.robin@gmail.com>
2015-01-20 20:05:22 +0100

    wip minify js

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,7 +1,8 @@
 var config = require('./config.js'),
     gulp = require('gulp'),
     nodemon = require('gulp-nodemon'),
-    less = require('gulp-less');
+    less = require('gulp-less'),
+    uglify = require('gulp-uglify');
 
 gulp.task('default', function () {
   nodemon({ script: 'server.js', ext: 'html js hbs less'})
@@ -16,3 +17,16 @@ gulp.task('less', function () {
     .pipe(less())
     .pipe(gulp.dest('./static/' + config.theme + '/css'));
 });
+
+gulp.task('js', function () {
+  // /static/bower_components/jquery/src/intro.js cannot be parsed and we do not need it
+  gulp.src(['./static/**/*.js', '!./static/bower_components/jquery/src/*'])
+    .pipe(uglify())
+    .on('error', function(){
+      console.log('####', arguments)
+    })
+    .pipe(gulp.dest('./static-dist/'));
+});
+
+
+
